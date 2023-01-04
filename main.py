da77769ae52c6cbde70fdf78c187c8d9ecf66383
Julia <jputz@flashnet.at>
2023-01-04 19:29:49 +0100

    add rules AvoidCollision + Accelerate

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from src.ca import *
-from src.rules import DummyShuffleRule
+from src.rules import DummyShuffleRule, Accelerate, AvoidCollision
 
 if __name__ == "__main__":
-    street = Street(1, 250, 20, 8)
-    rules = [DummyShuffleRule()]
+    v_max = 8
+    street = Street(1, 250, 20, v_max)
+    rules = [DummyShuffleRule(), Accelerate(v_max), AvoidCollision()]
     runner = Runner(street, rules)
     runner.run()
     print(len(runner.history))
--- src/rules.py
+++ src/rules.py
@@ -6,3 +6,52 @@ from ca import AbstractRule
 class DummyShuffleRule(AbstractRule):
     def apply(self, state: np.ndarray) -> np.ndarray:
         return np.roll(state, 1, axis=1)
+
+class Accelerate(AbstractRule):
+    """
+    increase vehicle speed if maximum is not yet reached
+    """
+    def __init__(self, v_max: int):
+        self.v_max = v_max
+
+    def apply(self, state: np.ndarray) -> np.ndarray:
+        check_velocity = (state < self.v_max) & (state >= 0)
+        state[check_velocity] += 1 
+        return state
+
+class AvoidCollision(AbstractRule):
+    """
+    reduce vehicle speed to gap size, 
+    if gap to next vehicle is smaller than its speed
+    """
+
+    def check_following_vehicles(self, state: np.ndarray, index: int, speed: int) -> np.ndarray:
+        """
+        returns array of which following cells contain a vehicle
+        """
+        #move current vehicle to front
+        shifted_state = np.roll(state, -index)
+        #get following states
+        following_states = shifted_state[1:speed+1]
+
+        return following_states >= 0
+
+    def get_gap(self, condition: np.ndarray) -> int:
+        """
+        return gap(nr of cells) between current and following vehicle
+        """
+        true_indices = [np.where(condition[j:])[0] for j in range(condition.shape[0])]
+        result = np.array([x[0] if x.size != 0 
+                                else int(condition[-1]) 
+                                for x in true_indices])
+        return result[0]
+
+    def apply(self, state: np.ndarray) -> np.ndarray:
+        for index, speed in enumerate(state):
+            if(speed>0):
+                following_vehicles = self.check_following_vehicles(state, index, speed)
+
+                if following_vehicles.any():
+                    state[index] = self.get_gap(following_vehicles)
+
+        return state
