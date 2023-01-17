4d72fb343d0db32fae08fab3c3fd936d5d591f15
Valentin Bauer <valentin.bauer@tuwien.ac.at>
2023-01-17 23:27:18 +0100

    Add fixed port to run the UI

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import panel as pn
 from src.ui import TrafficSimulationUI
 
+PORT = 8001
+
 if __name__ == "__main__":
     sim = TrafficSimulationUI()
-    pn.serve(sim.get_user_interface)
+    pn.serve(sim.get_user_interface, port=PORT)
